04c2581471e197b6784a92e801987ccac847582d
tulare <tulare.paxgalactica@gmail.com>
2018-11-12 14:33:09 +0100

    Refactoring package sources into src and adding tests

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
 # -*- encoding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
-from setuptools import setup, find_packages
+import setuptools
 
 # Get version without import module
-exec(compile(open('pk_core/version.py').read(),
+exec(compile(open('src/pk_core/version.py').read(),
              'pk_core/version.py', 'exec'))
 
-with open('README.md') as f :
-    readme = f.read()
-
-with open('LICENSE') as f :
-    license = f.read()
-
-setup(
-    name='pk-core',
-    version=__version__,
-    description='Core utils for python projects',
-    long_description=readme,
-    author='Tulare Regnus',
-    author_email='tulare.paxgalactica@gmail.com',
-    url='https://github.com/tulare/pk-core',
-    license=license,
-    packages=find_packages(exclude=('tests',)),
-    zip_safe=False,
-    #install_requires=[defined in setup.cfg]
+setuptools.setup(
+    version = __version__,
+    package_dir = {
+        '' : str('src')
+    },
 )
